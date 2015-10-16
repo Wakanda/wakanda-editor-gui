348edff017c4be6336d8a8fc04329ac303143fc9
yousfiSaad <yousfi.saad@gmail.com>
2015-10-16 11:04:46 +0100

    scriptEditor=>preview

--- plugins/preview/index.js
+++ plugins/preview/index.js
@@ -20,42 +20,10 @@ export default {
 				= document.createElement('script');
 		script.src = "http://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js";
 
-		this.script = IDE.GUID.documentEditor.document.getElementById('wakanda-script');
 		this.prevIframe = document.createElement('iframe');
-
-		// TODO: remove this
 		this.prevIframe.setAttribute('style', 'width: 100%; height: 600;');
 	},
 
-	editScript() {
-		let scriptEditorDiv = document.createElement('div');
-		let editor = ace.edit(scriptEditorDiv);
-		editor.setTheme("ace/theme/monokai");
-    editor.getSession().setMode("ace/mode/javascript");
-		editor.setValue(this.script.innerText);
-		editor.setOptions({
-	    maxLines: Infinity
-		});
-		editor.session.setNewLineMode("windows");
-
-		let _this = this;
-
-		bootbox.dialog({
-		  message: scriptEditorDiv,
-		  title: "Edit Script",
-		  buttons: {
-		    success: {
-		      label: "Save changes",
-		      className: "btn-success",
-		      callback: () => {
-						let content = editor.getSession().getValue();
-						_this.script.innerHTML = content;
-		      }
-		    }
-		  }
-		});
-
-	},
 	preview(){
 
 		let html = IDE.GUID.documentEditor.document.children[0];
@@ -83,21 +51,6 @@ export default {
 			});
 		});
 
-	},
-
-	onChange(args) {
-		let {canUndo, canRedo} = args;
-
-		if (canUndo) {
-			IDE.toolbar.swapItemClass("undo", "undo", "undo-enabled");
-		} else {
-			IDE.toolbar.swapItemClass("undo", "undo-enabled", "undo");
-		}
-		if (canRedo) {
-			IDE.toolbar.swapItemClass("redo", "redo", "redo-enabled");
-		} else {
-			IDE.toolbar.swapItemClass("redo", "redo-enabled", "redo");
-		}
 	}
 
 }
