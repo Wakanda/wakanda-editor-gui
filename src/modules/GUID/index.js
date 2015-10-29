84352790331f7ce14f7774c85f1934d35cbad33a
yousfiSaad <yousfi.saad@gmail.com>
2015-10-29 16:46:49 +0000

    remove onhashchange event

--- src/modules/GUID/index.js
+++ src/modules/GUID/index.js
@@ -22,97 +22,94 @@ var Module = {
 			var AttributesPanel = require("./Pannels/Attributes");
 			var ResponsiveSelector = require('./Pannels/ResponsiveSelector');
 			var DragulaManager = require('./DragulaManager');
+			
+			//TODO - URL of the iframe content
+			let path = './workspace/' + location.hash.substring(1);
 
-			window.onhashchange = ()=>{
-				//TODO - URL of the iframe content
-				let path = './workspace/' + location.hash.substring(1);
-
-				IDE.GUID.documentEditor = new Editor({
-					path
-				})
-				.onReady((documentEditor) => {
-					//NOTE DragulaManager must be initialized *before* UserInterface
-					IDE.GUID.dragulaManager = new DragulaManager({
-						documentEditor,
-						sourceContainerId: 'components'
-					});
-
-					IDE.GUID.userInterface = new UserInterface({
-						documentEditor
-					});
-					IDE.GUID.documentEditorBroker = documentEditor.broker;
-
-					// load Pannels
-					IDE.GUID.panels = {};
-					// Outline
-					IDE.GUID.panels.outline = new Outline({
-						containerId: 'outline',
-						documentEditor,
-						userInterface: IDE.GUID.userInterface
-					});
-					// Components
-					IDE.GUID.panels.components = new Components({
-						documentEditor,
-						containerId: 'components'
-					});
-					// angular panel
-					let angularPanel = IDE.GUID.panels.angularPanel = new Angular({
-						documentEditor,
-						containerId: 'angular',
-						userInterface: IDE.GUID.userInterface
-					});
-
-					IDE.GUID.panels.styling = new Styling({
-						containerId: 'styling',
-						documentEditor
-					});
-
-					IDE.GUID.panels.responsive = new ResponsiveSelector({
-						documentEditor,
-						containerId: 'responsiveButtonsList'
-					});
-
-					IDE.GUID.panels.attributesPanel = new AttributesPanel({
-						documentEditor,
-						containerId: 'attributes'
-					});
-
-					//undoRedoManagement
-					let plugin = 'GuidHistoryManager',
-						type = 'button';
-					let items = [{
-						action: 'undo',
-						name: 'undo',
-						plugin,
-						type
-					}, {
-						action: 'redo',
-						name: 'redo',
-						plugin,
-						type
-					}];
-					IDE.toolbar.addItems(items);
-					// console.log(items);
-
-					//debug infos
-					// documentEditor.onElementSelected(function(arg) {
-					// 	console.log(arg.element);
-					// });
-
-					// documentEditor.onDocumentSizeChange(function(a) {
-					// 	console.log(a);
-					// });
-
-					//to use it on devtool
-					window.d = documentEditor;
-					window.o = IDE.GUID.panels.outline;
-
-					// activate
-					loaded();
+			IDE.GUID.documentEditor = new Editor({
+				path
+			})
+			.onReady((documentEditor) => {
+				//NOTE DragulaManager must be initialized *before* UserInterface
+				IDE.GUID.dragulaManager = new DragulaManager({
+					documentEditor,
+					sourceContainerId: 'components'
 				});
-			}
 
-			window.onhashchange();
+				IDE.GUID.userInterface = new UserInterface({
+					documentEditor
+				});
+				IDE.GUID.documentEditorBroker = documentEditor.broker;
+
+				// load Pannels
+				IDE.GUID.panels = {};
+				// Outline
+				IDE.GUID.panels.outline = new Outline({
+					containerId: 'outline',
+					documentEditor,
+					userInterface: IDE.GUID.userInterface
+				});
+				// Components
+				IDE.GUID.panels.components = new Components({
+					documentEditor,
+					containerId: 'components'
+				});
+				// angular panel
+				let angularPanel = IDE.GUID.panels.angularPanel = new Angular({
+					documentEditor,
+					containerId: 'angular',
+					userInterface: IDE.GUID.userInterface
+				});
+
+				IDE.GUID.panels.styling = new Styling({
+					containerId: 'styling',
+					documentEditor
+				});
+
+				IDE.GUID.panels.responsive = new ResponsiveSelector({
+					documentEditor,
+					containerId: 'responsiveButtonsList'
+				});
+
+				IDE.GUID.panels.attributesPanel = new AttributesPanel({
+					documentEditor,
+					containerId: 'attributes'
+				});
+
+				//undoRedoManagement
+				let plugin = 'GuidHistoryManager',
+					type = 'button';
+				let items = [{
+					action: 'undo',
+					name: 'undo',
+					plugin,
+					type
+				}, {
+					action: 'redo',
+					name: 'redo',
+					plugin,
+					type
+				}];
+				IDE.toolbar.addItems(items);
+				// console.log(items);
+
+				//debug infos
+				// documentEditor.onElementSelected(function(arg) {
+				// 	console.log(arg.element);
+				// });
+
+				// documentEditor.onDocumentSizeChange(function(a) {
+				// 	console.log(a);
+				// });
+
+				//to use it on devtool
+				window.d = documentEditor;
+				window.o = IDE.GUID.panels.outline;
+
+				// activate
+				loaded();
+			});
+
 			//TODO remove this
 			let tree = document.getElementById('tree');
 			tree.hidden = true;
