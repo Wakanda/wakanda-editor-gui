a2622345be039a2e3f75818fa17d6d9b916c976b
Saad Yousfi <yousfi.saad@gmail.com>
2016-03-11 19:11:52 +0000

    load modules using promise
    When using IDE.Core.onReady … it is possible that the modules load
    before onReady is called
    but using the chaining of promises, we are sure that the modules are
    loaded in the order and even if the modules are loaded the callback
    (passed to onReady) will be executed

--- src/core.js
+++ src/core.js
@@ -10,39 +10,32 @@ class Core {
 			throw "Only one instance of core is allowed.";
 		}
 
-		this.activatedPlugins = [];
-		this._modules          = {};
+		this._modulesInstances          = {};
 
-		var _EventEmitter   = require('../lib/micro-events.js');
-		this.events = new _EventEmitter();
+		//not using all to keep the order
+		this._readyPromise = coreModules
+			.reduce( ( prevActivationPromise, currentModuleName ) => {
+				return prevActivationPromise.then( ( ) => {
+					return new Promise((resolve, reject) => {
+						let moduleContent = require(`./modules/${currentModuleName}/index.js`);
+						// TODO: make activate asynchrone
+						moduleContent.activate((currentModuleInstance) => {
+							console.log(`Core module ${currentModuleName} loaded`);
+							this._modulesInstances[currentModuleName] = currentModuleInstance
+							resolve();
+						});
+					});
+				})
+			}, Promise.resolve());
 
-		coreModules.forEach(function(moduleName){
-			console.log(moduleName);
-			var module = require(`./modules/${moduleName}/index.js`);
-
-			module.activate(function(){
-				that.activatedPlugins.push(moduleName);
-
-				if(that.activatedPlugins.length === coreModules.length){
-					that.events.emit("ready");
-				}
-			});
-		});
 	}
 
 	get(moduleName) {
-		return this._modules[moduleName];
-	}
-
-	_activateModule(moduleName) {
-		if (!this._modules[moduleName]) {
-			throw "Plugin \"" + moduleName + "\" doesn't exist.";
-		}
-		this._modules[moduleName].isActivated = true;
+		return this._modulesInstances[moduleName];
 	}
 
 	onReady(callback) {
-		this.events.on("ready", callback);
+		this._readyPromise.then(callback);
 	}
 }
 
