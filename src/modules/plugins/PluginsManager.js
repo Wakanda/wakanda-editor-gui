0aafd212206ca7c22585991eed65fe3beb2b03b0
Saad Yousfi <yousfi.saad@gmail.com>
2016-03-22 11:45:00 +0100

    not usint IDE. inside of a plugin

--- src/modules/plugins/PluginsManager.js
+++ src/modules/plugins/PluginsManager.js
@@ -1,38 +1,38 @@
 var IDE = window.IDE || {};
-
+// NOTE: using fileManager to load plugins
 class PluginsManager {
 	constructor(){
-		
+
 		var _EventEmitter   = require('../../../lib/micro-events.js');
 		this.events = new _EventEmitter();
-		
+
 		this.toolbarElements  = [];
-		this.plugins          = {};		
+		this.plugins          = {};
 	}
-	
+
 	get(pluginName) {
 		return this.plugins[pluginName]
 	}
-	
+
 	load(pluginName) {
 		this.plugins[pluginName]          = {};
 		this.plugins[pluginName].code     = require(`../../../plugins/${pluginName}/index.js`);
 		this.plugins[pluginName].manifest = require(`../../../plugins/${pluginName}/manifest.js`);
 	}
-	
+
 	loadMultiple(plugins) {
-		plugins.forEach((plugin)=>this.load(plugin));		
+		plugins.forEach((plugin)=>this.load(plugin));
 		/*
 		var that = this;
 		plugins.forEach(function(plugin){
 			that.load(plugin);
 		});
 		*/
-		
+
 		//For now the load is Synchronous
-		IDE.plugins.events.emit("all_loaded");
+		this.events.emit("all_loaded");
 	}
-	
+
 	activate(pluginName){
 		/*
 		 * Behaviour to be discussed
@@ -41,33 +41,33 @@ class PluginsManager {
 		try{
 			this.plugins[pluginName].code.activate();
 		}catch(e){
-			
+
 		}
-		
+
 		this.events.emit("plugin_activated", this.plugins[pluginName]);
 	}
-	
+
 	registerPluginToolbarElements(pluginName) {
 		var toolbarElements = this.plugins[pluginName].manifest.toolbar;
-		
+
 		toolbarElements.forEach((element)=>{
 			element.plugin = pluginName;
 			this.toolbarElements.push(element);
 		});
 	}
-	
+
 	getToolbarElements() {
 		return this.toolbarElements;
 	}
-	
+
 	onPluginActivated(callback){
 		this.events.on("plugin_activated", callback );
 	}
-	
+
 	onPluginsLoaded(callback){
 		this.events.on("all_loaded", callback );
 	}
-	
+
 	onPluginsActivated(callback){
 		this.events.on("all_activated", callback );
 	}
